828917327c00a2c9ae39bf5b0488895a04b45687
syjn99 <syjn99@gmail.com>
2024-05-28 14:09:43 +0900

    Add bellatrix & capella sszgen scripts

---
+++ util/sszgen/sszgen_capella.sh
@@ -0,0 +1,20 @@
+#!#!/bin/bash
+
+path_to_go_eth2_client=$(pwd)
+path_to_fastssz="${1}"
+
+# Navigate to the fastssz repository path
+cd $path_to_fastssz
+
+# Run the sszgen command
+go run sszgen/*.go -suffix ssz \
+  -include "${path_to_go_eth2_client}/spec/phase0","${path_to_go_eth2_client}/spec/altair",,"${path_to_go_eth2_client}/spec/bellatrix" \
+  --path "${path_to_go_eth2_client}/spec/capella" \
+  --objs BeaconBlockBody,BeaconBlock,BeaconState,BLSToExecutionChange,ExecutionPayload,ExecutionPayloadHeader,HistoricalSummary,SignedBeaconBlock,SignedBLSToExecutionChange,Withdrawal
+
+# Navigate to go_eth2_client repo
+cd $path_to_go_eth2_client
+cd spec/capella
+
+# Run goimports
+goimports -w beaconblockbody_ssz.go beaconblock_ssz.go beaconstate_ssz.go blstoexecutionchange_ssz.go executionpayloadheader_ssz.go executionpayload_ssz.go historicalsummary_ssz.go signedbeaconblock_ssz.go signedblstoexecutionchange_ssz.go withdrawal_ssz.go
