98f9a20f171d7cfa8af7e91b3f8bc5b4bf04b8d4
syjn99 <syjn99@gmail.com>
2024-05-28 14:04:18 +0900

    Add common sszgen script

---
+++ util/sszgen/sszgen.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <version> <path>"
+    exit 1
+fi
+
+version=$1
+path=$2
+
+# Check if the version is one of the allowed options
+case $version in
+    altair|bellatrix|capella)
+        # Remove ssz files in spec/$version
+        cd "spec/{$version}"
+        rm -rf *_ssz.go
+        cd ../..
+
+        # Run the corresponding version script with the provided path
+        sh ./sszgen_${version}.sh "$path"
+        ;;
+    *)
+        echo "Error: Invalid version specified. Choose from 'altair', 'bellatrix', or 'capella'."
+        exit 1
+        ;;
+esac
